710a9c0de1e7e89907cbcc92e74869e062a3599b
Ronal Melgar <melgar1w@gmail.com>
2023-07-20 15:22:36 -0600

    v-1

---
+++ src/componentes/Testinomios.js
@@ -0,0 +1,25 @@
+import React from "react";
+import '../hojas-de-estilo/Testimonios.css';
+
+function Testimonios(props) {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className='imagen-testimonio'
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt={`Foto ${props.nombre}`}
+      />
+      <div className='contenedor-testo-testimonio'>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className='cargo-testimonio'>
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonios;
